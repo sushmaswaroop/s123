424425e25a355b7190a84f190ffed5c4522c0127
sushmaswaroop <109276442+sushmaswaroop@users.noreply.github.com>
2022-08-11 14:12:32 +0530

    Update login.py

--- login.py
+++ login.py
@@ -2,7 +2,7 @@ username = input(' enter your username: ')
 if username == 'Put Your Username Here':
    print("checking username")
    time.sleep(0.5)
-   print("username is right")
+   print("username is right...")
    password = input(' type your password: ')
    if password == 'Put Your Password Here':
     time.sleep(0.5)
